1773da5be76b9a374b273ac78db5f181e1899753
shivamsssm <stt91625@gmail.com>
2025-04-02 22:51:06 +0530

    Update Windowsetup.sh

--- Windowsetup.sh
+++ Windowsetup.sh
@@ -3,10 +3,10 @@ sudo apt update -y
 sudo apt install apache2 ufw p7zip-full qemu-system-x86-64 -y
 sudo ufw allow 'VNC'
 sudo ufw status
-if [ -e "./XP.qcow2" ]; then
+if [ -e "./win.qcow2" ]; then
     echo "File exists"
 else
-    wget -O XP.qcow2 'https://dl.dropboxusercontent.com/scl/fi/1otebde93u9emo7brtuc9/xp.qcow2?rlkey=7suq01uor2sdesqqt8umf6op1&st=nw3h0e9r&dl=1'
+    wget -O win.qcow2 'https://dl.dropboxusercontent.com/scl/fi/1otebde93u9emo7brtuc9/xp.qcow2?rlkey=7suq01uor2sdesqqt8umf6op1&st=nw3h0e9r&dl=1'
    
     
 fi
@@ -24,9 +24,9 @@ echo "----------\/----------"
 echo "Please go to the following link to check if the generated address is working and connect using VNC: https://dashboard.ngrok.com/agents"
 echo "----------/\----------"
 sleep 5
-sudo qemu-system-x86_64 -cpu core2duo,+avx -usb -device usb-kbd -device usb-tablet -smp sockets=1,cores=4,threads=1 -m 512M -hda XP.qcow2 -vga vmware -device ac97 -device e1000,netdev=n0 -netdev user,id=n0 -accel tcg,thread=multi,tb-size=2048 -vnc :0
+sudo qemu-system-x86_64 -cpu core2duo,+avx -usb -device usb-kbd -device usb-tablet -smp sockets=1,cores=4,threads=1 -m 512M -hda win.qcow2 -vga vmware -device ac97 -device e1000,netdev=n0 -netdev user,id=n0 -accel tcg,thread=multi,tb-size=2048 -vnc :0
 clear
 echo "To run again, run the following command:"
 echo "----------\/----------"
-echo "rungrok="\$\(./ngrok tcp 5900\)" & sudo qemu-system-x86_64 -cpu core2duo,+avx -usb -device usb-kbd -device usb-tablet -smp sockets=1,cores=4,threads=1 -m 512M -hda XP.qcow2 -vga vmware -device ac97 -device e1000,netdev=n0 -netdev user,id=n0 -accel tcg,thread=multi,tb-size=2048 -vnc :0"
+echo "rungrok="\$\(./ngrok tcp 5900\)" & sudo qemu-system-x86_64 -cpu core2duo,+avx -usb -device usb-kbd -device usb-tablet -smp sockets=1,cores=4,threads=1 -m 512M -hda win.qcow2 -vga vmware -device ac97 -device e1000,netdev=n0 -netdev user,id=n0 -accel tcg,thread=multi,tb-size=2048 -vnc :0"
 echo "----------/\----------"
